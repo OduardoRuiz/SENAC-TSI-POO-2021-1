f849da01096e49b4b80066ed52968a72dd1be149
OduardoRuiz <carloseduardoruizs@gmail.com>
2021-04-26 18:04:01 -0300

    Aula-13-04

---
+++ poo/praticando_poo/classes/abstratas/TipoPessoa.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'DataBase.class.php';
+
+abstract class TipoPessoa extends DataBase
+{
+
+    protected $id;
+    protected $nome;
+
+    public function __construct()
+    {
+
+        parent::__construct();
+    }
+}
---
+++ poo/praticando_poo/estoque.php
@@ -0,0 +1,90 @@
+<?php
+
+require('classes/Usuario.class.php');
+require('classes/Fabricante.class.php');
+require('classes/Estoque.class.php');
+require('classes/Movimentacao.class.php');
+
+class Main
+{
+
+    public function __construct()
+    {
+
+        //apenas teste
+        echo "\n\n--- COMEÇO DO PROGRAMA ---\n\n";
+
+        $objUsuario = new Usuario;
+        $objFabricante = new Fabricante;
+        $objEstoque = new Estoque;
+        $objMovimentacao = new Movimentacao;
+
+        switch ($_SERVER['argv'][1]) {
+
+            case 'gravaUsuario':
+
+                $this->gravaUsuario($objUsuario);
+                break;
+
+            case 'editaUsuario':
+
+                $this->editaUsuario($objUsuario);
+                break;
+
+            default:
+
+                echo "\nERRO: Não existe a funcionalidade {$_SERVER['argv'][1]}\n";
+        }
+    }
+
+    public function editaUsuario($objUsuario)
+    {
+
+        $dados = $this->tratarDados();
+
+        $objUsuario->setDados($dados);
+
+        if ($objUsuario->gravarDados()) {
+
+            echo "\nUsuário editado com sucesso!\n";
+        }
+    }
+
+    public function gravaUsuario($objUsuario)
+    {
+
+        $dados = $this->tratarDados();
+
+        $objUsuario->setDados($dados);
+
+        if ($objUsuario->gravarDados()) {
+
+            echo "\nUsuário gravado com sucesso!\n";
+        }
+    }
+
+    public function tratarDados()
+    {
+
+        $args = explode(',', $_SERVER['argv'][2]); //dados passados pelo usuário na linha de 	comando
+
+        foreach ($args as $valor) {
+
+            $arg = explode('=', $valor);
+
+            $dados[$arg[0]] = $arg[1];
+        }
+
+        return $dados;
+    }
+
+    public function __destruct()
+    {
+
+        sleep(1);
+
+        echo "\n\n--- FIM DO PROGRAMA ---\n\n";
+    }
+}
+
+new Main;
